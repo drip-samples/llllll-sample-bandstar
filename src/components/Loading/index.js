16f7395080714cc00e6f039726711f9dc4a6f17c
Ogawa Mitsunori <mitsunori.ogawa@drecom.co.jp>
2018-10-15 11:33:42 +0900

    A: loading and metamask errors components

---
+++ src/components/Loading/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import CircularProgress from '@material-ui/core/CircularProgress'
+
+export default () => (
+  <div>
+    Loading...
+    <CircularProgress size={20} />
+  </div>
+)
---
+++ src/components/MetamaskNotFound/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default () => (
+  <div>
+    Metamask Not Found
+  </div>
+)
