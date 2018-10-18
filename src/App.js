179f59ced87fadbd58a3727e55e276f4b8791d03
Ogawa Mitsunori <mitsunori.ogawa@drecom.co.jp>
2018-10-18 14:05:35 +0900

    A: Footer

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import GithubPagesRedirectProvider from './providers/GithubPagesRedirectProvider
 import TokenDetail from './views/TokenDetail'
 import MyPage from './views/MyPage'
 import Header from './components/Header'
+import Footer from './components/Footer'
 import NotFound from './components/NotFound'
 
 class App extends Component {
@@ -16,11 +17,14 @@ class App extends Component {
           <GithubPagesRedirectProvider>
             <React.Fragment>
               <Header />
-              <Switch>
-                <Route exact path="/:networkName/tokens/:tokenId" component={TokenDetail} />
-                <Route exact path="/" component={MyPage} />
-                <Route component={NotFound} />
-              </Switch>
+              <div style={{paddingBottom: `${Footer.height()}px`}}>
+                <Switch>
+                  <Route exact path="/:networkName/tokens/:tokenId" component={TokenDetail} />
+                  <Route exact path="/" component={MyPage} />
+                  <Route component={NotFound} />
+                </Switch>
+              </div>
+              <Footer />
             </React.Fragment>
           </GithubPagesRedirectProvider>
         </BrowserRouter>
